53f5a5c28b13fd6bee293e02cc6d9797f42b8826
amanramoliya <19ceuot073@ddu.ac.in>
2023-03-15 17:26:21 +0530

    ENH: adds signup page with css

--- frontend/src/components/auth/SignUp.tsx
+++ frontend/src/components/auth/SignUp.tsx
@@ -1,5 +1,50 @@
+import "../../css/auth/SignUp.css";
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <>
+      <div className="SignUpContainer">
+        <div className="Leftside">
+          <img
+            className="Image"
+            src="https://wallpapercave.com/wp/wp6480749.jpg"
+          ></img>
+        </div>
+
+        <div className="Rightside">
+          <div className="RightContent">
+            <div className="text-3xl mb-5">Sign Up</div>
+
+            <form>
+              <label className="FormItems">
+                <input type="text" placeholder="Email" className="FormInput" />
+
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="FormInput"
+                />
+
+                <input
+                  type="password"
+                  placeholder="Confirm Password"
+                  className="FormInput"
+                />
+
+                <button className="Submit">Sign Up</button>
+
+                <label className="AlreadyAccount">
+                  Already have an account?{" "}
+                  <a className="SignIn" href="/auth/signin">
+                    Sign In
+                  </a>
+                </label>
+              </label>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SignUp;
