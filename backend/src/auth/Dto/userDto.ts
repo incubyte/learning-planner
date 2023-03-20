3f0443cccab80214383c347a7f189b666dcce572
Shreyas <mandlikshreyas01@gmail.com>
2023-03-20 16:39:29 +0530

    adds husky

--- backend/src/auth/Dto/userDto.ts
+++ backend/src/auth/Dto/userDto.ts
@@ -3,7 +3,9 @@ import { IsEmail, IsNotEmpty, Matches } from 'class-validator';
 export class UserDto {
   @IsEmail()
   @IsNotEmpty()
-  @Matches('^[a-zA-Z0-9+_.-]+@[incubyte.co]+$',"",{"message":"Email Id should be an Incubyte Email-Id. "})
+  @Matches('^[a-zA-Z0-9+_.-]+@[incubyte.co]+$', '', {
+    message: 'Email Id should be an Incubyte Email-Id. ',
+  })
   email: string;
 
   @IsNotEmpty()
