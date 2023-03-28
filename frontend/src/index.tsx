d4be9e3960df29395da3ab78bd879fb65dfef9ed
amanramoliya <19ceuot073@ddu.ac.in>
2023-03-28 11:54:55 +0530

    ENH: adds test for home page

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,7 +6,6 @@ import SignIn from "./components/auth/SignIn";
 import SignUp from "./components/auth/SignUp";
 import HomePage from "./components/HomePage";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -43,8 +42,3 @@ root.render(
     <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
