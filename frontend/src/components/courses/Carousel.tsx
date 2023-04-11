a025238b4204b4c9fb4a2d5fa69f149c4060e596
Aayush Prajapati <ashprajapati67@gmail.com>
2023-04-11 15:19:31 +0530

    ENH:adds Carousel and Card component

---
+++ frontend/src/components/courses/Carousel.tsx
@@ -0,0 +1,66 @@
+import CourseCard from "./CourseCard";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import "../../css/courses/Carousel.css";
+interface carouselProps {
+  titleName?: string;
+  contentId: string;
+  courses: {
+    courseImage: string;
+    courseName: string;
+  }[];
+}
+
+const Carousel = ({ titleName, courses, contentId }: carouselProps) => {
+  const scrollLeft = () => {
+    const content = document.getElementById(contentId);
+    if (content !== null) {
+      content.scrollLeft -= 300;
+    }
+  };
+  const scrollRight = () => {
+    const content = document.getElementById(contentId);
+    if (content !== null) {
+      content.scrollLeft += 300;
+    }
+  };
+  return (
+    <div className="relative" role={contentId}>
+      <div className="carouselTitleName" data-testid="carouselTitleName">
+        {titleName}
+      </div>
+      <div className="buttons">
+        <button
+          onClick={scrollLeft}
+          className="button"
+          data-testid="scrollLeft"
+        >
+          <FiChevronLeft />
+        </button>
+        <button
+          onClick={scrollRight}
+          className="button"
+          data-testid="scrollRight"
+        >
+          <FiChevronRight />
+        </button>
+      </div>
+      <div
+        id={contentId}
+        className="carouselListContent"
+        data-testid="carouselContent"
+      >
+        {courses.map((course, index) => {
+          return (
+            <CourseCard
+              key={index}
+              courseImage={course.courseImage}
+              courseName={course.courseName}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
---
+++ frontend/src/components/courses/CourseCard.tsx
@@ -0,0 +1,39 @@
+import "../../css/courses/CourseCard.css";
+import Button from "../utilities/Button";
+interface courseCardProps {
+  courseImage: string;
+  courseName: string;
+}
+const CourseCard = ({ courseImage, courseName }: courseCardProps) => {
+  return (
+    <>
+      <div className="carouselCardContainer">
+        <div className="carouselCardInnerContainer">
+          <div className="courseCardImageContainer">
+            <img
+              src={courseImage}
+              alt="course"
+              className="object-fit object-center courseCardImage"
+              data-testid="courseCardImage"
+            />
+            <div className="courseCardCourseNameContainer">
+              <div
+                className="courseCardCourseName"
+                data-testid="courseCardName"
+              >
+                {courseName}
+              </div>
+            </div>
+            <div className="courseCardButtonContainer">
+              <Button title="Explore" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+ 
+export default CourseCard;
+ 
+ 
