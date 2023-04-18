1c8200742e2920b5c47ea4f32dad63e8b225ccfd
amanramoliya <19ceuot073@ddu.ac.in>
2023-04-18 16:31:52 +0530

    ENH: adds function to fetch user details

--- frontend/src/components/user/Profile.tsx
+++ frontend/src/components/user/Profile.tsx
@@ -1,4 +1,26 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Profile = () => {
+  const navigator = useNavigate();
+
+  const fetchUser = async () => {
+    const response = await fetch("https://backend-mu-plum.vercel.app/user", {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    });
+    if (response.ok) {
+      console.log(response);
+    } else {
+      navigator("/auth/signin");
+    }
+  };
+
+  const authToken = localStorage.getItem("authToken");
+  useEffect(() => {
+    // fetchUser();
+  });
   return <div>Profile</div>;
 };
 
