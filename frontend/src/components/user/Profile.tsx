501c00feeeb3e499d6e6224e9f5558c75d647276
Charvit123 <charvitardeshna@gmail.com>
2023-04-19 18:39:26 +0530

    ENH:adds designing to profile page

--- frontend/src/components/user/Profile.tsx
+++ frontend/src/components/user/Profile.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Carousel from "../utilities/Carousel";
 import Navbar from "../utilities/Navbar";
+import "../../css/user/profile.css";
 
 const Profile = () => {
   const navigator = useNavigate();
@@ -31,7 +32,7 @@ const Profile = () => {
         isProfile={false}
         isSearch={false}
       ></Navbar>
-      <div className="text-5xl lg:text-4xl md:text-7xl xsm:text-4xl mb-5 grid grid-cols-1 justify-items-center content-center object-contain">
+      <div className="text-5xl lg:text-4xl md:text-5xl xsm:text-4xl mb-5 grid grid-cols-1 justify-items-center content-center object-contain">
         My Profile
       </div>
       <div className="grid grid-cols-1 justify-items-center content-center object-contain">
@@ -40,23 +41,25 @@ const Profile = () => {
           src="https://wallpapercave.com/wp/wp6480749.jpg"
         ></img>
       </div>
-      <div className="grid grid-cols-2 justify-items-center content-center object-contain">
-        <label>Email</label>
-        <label>Employee Id</label>
-      </div>
-      <div className="grid grid-cols-2 justify-items-center content-center object-contain">
-        <input disabled value="email"></input>
-        <input disabled value="EID"></input>
-      </div>
-      <div className="grid grid-cols-3 justify-items-center content-center object-contain">
-        <label>Client Team</label>
-        <label>Role</label>
-        <label>Total Credit</label>
-      </div>
-      <div className="grid grid-cols-3 justify-items-center content-center object-contain">
-        <input disabled value="Client Team"></input>
-        <input disabled value="Role"></input>
-        <input disabled value="Credit"></input>
+      <div className="grid grid-cols-1">
+        <div className="grid grid-cols-2 justify-items-center content-center object-contain">
+          <label className="ProfileLabel">Email</label>
+          <label className="ProfileLabel">Employee Id</label>
+        </div>
+        <div className="grid grid-cols-2 justify-items-center content-center object-contain">
+          <input disabled value="Email" className="ProfileInput"></input>
+          <input disabled value="EID" className="ProfileInput"></input>
+        </div>
+        <div className="grid grid-cols-3 justify-items-center content-center object-contain">
+          <label className="ProfileLabel">Client Team</label>
+          <label className="ProfileLabel">Role</label>
+          <label className="ProfileLabel">Total Credit</label>
+        </div>
+        <div className="grid grid-cols-3 justify-items-center content-center object-contain">
+          <input disabled value="Client Team" className="ProfileInput"></input>
+          <input disabled value="Role" className="ProfileInput"></input>
+          <input disabled value="Credit" className="ProfileInput"></input>
+        </div>
       </div>
       <Carousel
         titleName="My Courses"
