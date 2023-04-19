84bd9535ea79d2be79ee97991ac99941b90c496c
Shreyas <mandlikshreyas01@gmail.com>
2023-04-19 16:14:09 +0530

    ENH:adds the service and its test

--- backend/src/user/user.service.spec.ts
+++ backend/src/user/user.service.spec.ts
@@ -1,18 +1,60 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { PrismaService } from '@/prisma/prisma.service';
+import { UserDto } from '@/auth/dto/user.dto';
 
 describe('UserService', () => {
   let service: UserService;
-
+  let prismaService: PrismaService;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: PrismaService,
+          useFactory() {
+            return {
+              user: {
+                findFirst: jest.fn(),
+              },
+            };
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('User Service', () => {
+    const userDTO: UserDto = {
+      email: 'john@incubyte.co',
+      password: '1234',
+    };
+    it('should return user of specified id', async () => {
+      const mockResponse = {
+        email: userDTO.email,
+        password: userDTO.password,
+        id: '1',
+        createdAt: Date.prototype,
+        profilePhoto: 'https://profilephoto.com',
+        updatedAt: Date.prototype,
+        eId: 'E0001',
+        role: 'BQA',
+        clientTeam: 'abc',
+      };
+
+      jest
+        .spyOn(prismaService.user, 'findFirst')
+        .mockResolvedValue(mockResponse);
+      const result = await service.getUserById('1');
+      expect(prismaService.user.findFirst).toBeCalledTimes(1);
+      expect(result).toMatchObject(mockResponse);
+    });
   });
 });
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,12 +1,11 @@
+import { PrismaService } from '@/prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class UserService {
-  getUserById(
-    id: string,
-  ):
-    | import('.prisma/client').User
-    | PromiseLike<import('.prisma/client').User> {
-    throw new Error('Method not implemented.');
+  constructor(private readonly prismaService: PrismaService) {}
+  async getUserById(id: string): Promise<User> {
+    return this.prismaService.user.findFirst({ where: { id } });
   }
 }
