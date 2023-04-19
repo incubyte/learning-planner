c627718a87e540f3c1ab0fbcd23c312b5d2314f3
amanramoliya <19ceuot073@ddu.ac.in>
2023-04-19 10:01:56 +0530

    ENH: adds carousel for active course in profile page

--- frontend/src/components/user/Profile.tsx
+++ frontend/src/components/user/Profile.tsx
@@ -1,8 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Carousel from "../utilities/Carousel";
 
 const Profile = () => {
   const navigator = useNavigate();
+
+  const [activeCourse, setActiveCourse] = useState<any[]>([]);
 
   const fetchUser = async () => {
     const response = await fetch("https://backend-mu-plum.vercel.app/user", {
@@ -21,7 +24,17 @@ const Profile = () => {
   useEffect(() => {
     // fetchUser();
   });
-  return <div>Profile</div>;
+  return (
+    <>
+      {" "}
+      <div>Profile</div>
+      <Carousel
+        titleName="My Courses"
+        courses={activeCourse}
+        contentId="popContent"
+      />
+    </>
+  );
 };
 
 export default Profile;
