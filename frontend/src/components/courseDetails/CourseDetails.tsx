3d0b2b52c270da6ca857d52e88c0150e6eef132b
Charvit123 <charvitardeshna@gmail.com>
2023-05-04 18:55:50 +0530

    ENH:adds responsiveness

--- frontend/src/components/courseDetails/CourseDetails.tsx
+++ frontend/src/components/courseDetails/CourseDetails.tsx
@@ -52,34 +52,31 @@ const CourseDetails = () => {
       ></Navbar>
 
       <div className="grid justify-items-center content-center object-contain p-10">
-        <div className="container grid justify-items-center content-center object-contain bg-slate-100 rounded-lg">
+        <div className="container grid justify-items-center content-center object-contain bg-slate-100 rounded-lg max-w-full">
           <div className="relative mt-5">
             <img
               data-testid="courseImage"
-              className="rounded-full h-40 w-40 block"
+              className="rounded-full lg:h-40 md:h-36 sm:h-24 xsm:h-24 lg:w-40 md:w-36 sm:w-24 xsm:w-24 block"
               src={course?.imageUrl}
             ></img>
           </div>
           <div
             data-testid="courseName"
-            className="text-5xl lg:text-4xl md:text-4xl xsm:text-4xl mt-4"
+            className="text-5xl lg:text-4xl md:text-3xl sm:text-3xl xsm:text-2xl mt-4"
           >
             {course?.name}
           </div>
-          <div
-            data-testid="courseTags"
-            className="my-5 place-items-center w-full"
-          >
+          <div data-testid="courseTags" className="my-5 place-items-center">
             {course?.tags.map((tag, index) => (
               <button
                 key={index}
-                className="hover:cursor-default rounded-full inline-block whitespace-nowrap bg-gradient-to-r from-sky-300 to-indigo-300 px-[0.20em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-xs md:text-sm font-bold leading-none my-1 md:my-0 md:mr-2 md:mt-3 w-[120px] text-black"
+                className="hover:cursor-default rounded-full inline-block whitespace-nowrap bg-gradient-to-r from-sky-300 to-indigo-300 px-[0.20em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-xs md:text-sm lg:text-sm sm:text-sm xsm:text-sm font-bold leading-none my-1 md:my-0 sm:my-0 xsm:my-1 xsm:mx-1 md:mx-1 sm:mx-1 md:mt-3 sm:mt-3 xsm:mt-2 w-[120px] text-black"
               >
                 {tags[tag - 1]?.name}
               </button>
             ))}
           </div>
-          <div className="container w-10/12 bg-slate-300 rounded-lg">
+          <div className="container w-10/12 xsm:max-w-full bg-slate-300 rounded-lg">
             <div data-testid="courseDescription" className="m-12">
               <p>{course?.description}</p>
             </div>
