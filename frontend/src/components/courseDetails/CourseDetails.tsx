be6eebf61410cb842607aed23ae5bf9011a2b2d3
Charvit123 <charvitardeshna@gmail.com>
2023-05-08 18:04:01 +0530

    ENH:changes in test case

--- frontend/src/components/courseDetails/CourseDetails.tsx
+++ frontend/src/components/courseDetails/CourseDetails.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import { courseType } from "../courses/Courses";
+import "../../css/courseDetails/courseDetails.css";
 import Navbar from "../utilities/Navbar";
 
 const CourseDetails = () => {
@@ -133,42 +134,36 @@ const CourseDetails = () => {
         isSearch={false}
       ></Navbar>
 
-      <div className="grid justify-items-center content-center object-contain p-10">
-        <div className="container grid justify-items-center content-center object-contain bg-slate-100 rounded-lg max-w-full">
-          <div className="relative mt-5">
+      <div className="courseDetailsMainPage">
+        <div className="courseDetailsContainer">
+          <div className="courseImageDiv">
             <img
               data-testid="courseImage"
-              className="rounded-full lg:h-40 md:h-36 sm:h-24 xsm:h-24 lg:w-40 md:w-36 sm:w-24 xsm:w-24 block"
+              className="courseImage"
               src={course?.imageUrl}
             ></img>
           </div>
-          <div
-            data-testid="courseName"
-            className="text-5xl lg:text-4xl md:text-3xl sm:text-3xl xsm:text-2xl mt-4"
-          >
+          <div data-testid="courseName" className="courseHeader">
             {course?.name}
           </div>
-          <div data-testid="courseTags" className="my-5 place-items-center">
+          <div data-testid="courseTags" className="courseTagsDiv">
             {course?.tags.map((tag, index) => (
-              <button
-                key={index}
-                className="hover:cursor-default rounded-full inline-block whitespace-nowrap bg-gradient-to-r from-sky-300 to-indigo-300 px-[0.20em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-xs md:text-sm lg:text-sm sm:text-sm xsm:text-sm font-bold leading-none my-1 md:my-0 sm:my-0 xsm:my-1 xsm:mx-1 md:mx-1 sm:mx-1 md:mt-3 sm:mt-3 xsm:mt-2 w-[120px] text-black"
-              >
+              <button key={index} className="courseTags">
                 {tags[tag - 1]?.name}
               </button>
             ))}
           </div>
-          <div className="container w-10/12 xsm:max-w-full bg-slate-300 rounded-lg mb-4">
+          <div className="courseDescriptionContainer">
             <div data-testid="courseDescription" className="m-12">
               <p>{course?.description}</p>
             </div>
           </div>
           {!isCompleted && (
-            <div data-testid="courseButton" className="my-8">
+            <div data-testid="courseButton" className="mb-4 mt-2">
               {!isEnrolled && (
                 <button
                   data-testid="courseEnroll"
-                  className="text-white font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg mr-1 mb-1 ease-linear transition-all duration-150 bg-emerald-500 active:bg-emerald-600"
+                  className="courseButtons"
                   onClick={enrollCourse}
                 >
                   Enroll
@@ -176,7 +171,8 @@ const CourseDetails = () => {
               )}
               {isEnrolled && (
                 <button
-                  className="text-white font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg mr-1 mb-1 ease-linear transition-all duration-150 bg-emerald-500 active:bg-emerald-600"
+                  data-testid="courseComplete"
+                  className="courseButtons"
                   onClick={completeCourse}
                 >
                   Complete Course
--- frontend/src/test/EnrollCourseIntegration.test.tsx
+++ frontend/src/test/EnrollCourseIntegration.test.tsx
@@ -17,52 +17,47 @@ afterEach(() => {
 function sleep(ms: any) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
-beforeAll(async () => {
-  render(
-    <BrowserRouter>
-      <SignIn />
-    </BrowserRouter>
-  );
-
-  const signInButton = screen.getByTestId("signinButton") as HTMLButtonElement;
-  const signInEmail = screen.getByTestId("signinEmail") as HTMLInputElement;
-
-  const signInPassword = screen.getByTestId(
-    "signinPassword"
-  ) as HTMLInputElement;
-
-  await act(() => {
-    fireEvent.change(signInEmail, {
-      target: { value: "aman.r@incubyte.co" },
-    });
-  });
-
-  await act(() => {
-    fireEvent.change(signInPassword, { target: { value: "Aman@111" } });
-  });
-
-  await act(() => {
-    fireEvent.click(signInButton);
-  });
-});
-
 describe("test course detail", () => {
   jest.setTimeout(30000);
   it("Accessing courses detail Page", async () => {
-    const mockId = "14463832-3393-4515-ab56-0343efdbbf97";
+    const mockId = "bacd123445afha";
     render(
       <MemoryRouter initialEntries={[`/course/${mockId}`]}>
+        <SignIn />
         <Routes>
           <Route path="/course/:id" element={<CourseDetails />}></Route>
         </Routes>
       </MemoryRouter>
     );
 
+    const signInButton = screen.getByTestId(
+      "signinButton"
+    ) as HTMLButtonElement;
+    const signInEmail = screen.getByTestId("signinEmail") as HTMLInputElement;
+
+    const signInPassword = screen.getByTestId(
+      "signinPassword"
+    ) as HTMLInputElement;
+
+    await act(() => {
+      fireEvent.change(signInEmail, {
+        target: { value: "aman.r@incubyte.co" },
+      });
+    });
+
+    await act(() => {
+      fireEvent.change(signInPassword, { target: { value: "Aman@111" } });
+    });
+
+    await act(() => {
+      fireEvent.click(signInButton);
+    });
+
     const enrollButton = screen.getByTestId(
       "courseEnroll"
     ) as HTMLButtonElement;
 
-    await act(() => {
+    await act(async () => {
       fireEvent.click(enrollButton);
     });
     await sleep(10000);
