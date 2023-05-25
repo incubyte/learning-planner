8d8b43f8c944bc309e6e67fcb3213d7b32f2f0b2
amanramoliya <19ceuot073@ddu.ac.in>
2023-05-25 11:42:18 +0530

    ENH: adds table to display all users

--- admin/src/components/user/Users.tsx
+++ admin/src/components/user/Users.tsx
@@ -1,6 +1,29 @@
+import { useEffect, useState } from "react";
 import Navbar from "../utilities/Navbar";
+import { userType } from "./user";
 
 const Users = () => {
+  const [allUsers, setAllUsers] = useState<userType[]>();
+  const authToken = localStorage.getItem("authToken");
+  const fetchUsers = async () => {
+    const response = await fetch(
+      "https://backend-mu-plum.vercel.app/user/all",
+      {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      }
+    );
+
+    if (response.ok) {
+      const fetchUsers = await response.json();
+      setAllUsers(fetchUsers);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
   return (
     <>
       <Navbar
@@ -9,6 +32,58 @@ const Users = () => {
         isProfile={true}
         isUser={false}
       ></Navbar>
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="w-full text-sm text-left text-gray-500">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-200">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                EId
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Email
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Designation
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Role
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {allUsers?.map((user, index) => {
+              return (
+                <tr
+                  key={index}
+                  className={`${index % 2 === 1 ? "bg-gray-100" : "bg-white"}`}
+                  role="row"
+                >
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                  >
+                    {user.eId}
+                  </th>
+                  <td className="px-6 py-4">{user.email}</td>
+                  <td className="px-6 py-4">{user.role}</td>
+                  <td className="px-6 py-4">{user.roles}</td>
+                  <td className="px-6 py-4">
+                    <a
+                      href="#"
+                      className="font-medium text-blue-600 hover:underline"
+                    >
+                      Info
+                    </a>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
