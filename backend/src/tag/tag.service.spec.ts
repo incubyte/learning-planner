0e8bee4d55b3ab95e6ec17055f498115fe311ba3
Aayush Prajapati <ashprajapati67@gmail.com>
2023-05-30 15:02:54 +0530

    ENH:adds the service for create tag with test

--- backend/src/tag/tag.service.spec.ts
+++ backend/src/tag/tag.service.spec.ts
@@ -2,6 +2,7 @@ import { PrismaService } from '@Prisma/prisma.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TagDto } from '@Tag/dto/tag.dto';
 import { TagService } from '@Tag/tag.service';
+import { BadRequestException } from '@nestjs/common';
 
 describe('TagService', () => {
   let service: TagService;
@@ -78,5 +79,41 @@ describe('TagService', () => {
     const result = await service.getById('1');
     expect(prismaService.tag.findFirst).toBeCalledTimes(1);
     expect(result).toMatchObject(mockResponse);
+  });
+
+  it('should create a new tag if it is not present', async () => {
+    const tag: TagDto = {
+      name: 'Tag',
+    };
+    const responseTag = {
+      id: 1,
+      name: 'Tag',
+    };
+    jest.spyOn(prismaService.tag, 'findFirst').mockResolvedValue(null);
+    jest.spyOn(prismaService.tag, 'create').mockResolvedValueOnce(responseTag);
+    const result = await service.createTag(tag);
+    expect(prismaService.tag.create).toHaveBeenCalledWith({
+      data: {
+        name: tag.name,
+      },
+    });
+    expect(prismaService.tag.create).toHaveBeenCalledTimes(1);
+    expect(result).toEqual(responseTag);
+  });
+
+  it('should throw BadRequestExecption if tag is already present', async () => {
+    const tag: TagDto = {
+      name: 'Tag',
+    };
+    const responseTag = {
+      id: 1,
+      name: 'Tag',
+    };
+    jest.spyOn(prismaService.tag, 'findFirst').mockResolvedValue(responseTag);
+    const response = new BadRequestException('Tag already exist');
+    jest.spyOn(prismaService.tag, 'create').mockRejectedValue(response);
+    await expect(service.createTag(tag)).rejects.toThrow(
+      new BadRequestException('Tag already exist'),
+    );
   });
 });
--- backend/src/tag/tag.service.ts
+++ backend/src/tag/tag.service.ts
@@ -1,13 +1,10 @@
 import { PrismaService } from '@Prisma/prisma.service';
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { Tag } from '@prisma/client';
 import { TagDto } from './dto/tag.dto';
 
 @Injectable()
 export class TagService {
-  createTag(tagDTo: TagDto): Tag | PromiseLike<Tag> {
-    throw new Error('Method not implemented.');
-  }
   constructor(private readonly prismaService: PrismaService) {}
 
   async getAll(): Promise<Tag[]> {
@@ -16,5 +13,17 @@ export class TagService {
   async getById(id: string): Promise<Tag> {
     const intId = Number.parseInt(id);
     return await this.prismaService.tag.findFirst({ where: { id: intId } });
+  }
+  async createTag(tag: TagDto): Promise<Tag> {
+    const prismaTag = await this.prismaService.tag.findFirst({
+      where: { name: tag.name },
+    });
+    if (prismaTag) {
+      throw new BadRequestException('Tag already exist');
+    }
+    const createTag = await this.prismaService.tag.create({
+      data: { name: tag.name },
+    });
+    return createTag;
   }
 }
