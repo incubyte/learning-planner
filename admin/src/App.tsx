451bae63f95f16b732fb61277f471675c81f32f0
amanramoliya <19ceuot073@ddu.ac.in>
2023-05-30 16:54:34 +0530

    ENH: adds authentication to admin app

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,7 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 function App() {
+  const navigator = useNavigate();
+
+  const [page, setPage] = useState([]);
+  const fetchPage = async () => {
+    const response = await fetch("https://backend-mu-plum.vercel.app/", {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    });
+    if (!response.ok) {
+      navigator("/auth/signin");
+    }
+  };
+  const authToken = localStorage.getItem("authToken");
+  useEffect(() => {
+    fetchPage();
+  }, page);
   return (
     <>
       <Outlet></Outlet>
