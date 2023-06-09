3235eaaea17dd22f5b4d4aad605f6beda109e6c1
amanramoliya <19ceuot073@ddu.ac.in>
2023-06-09 10:25:09 +0530

    ENH: removes unused code from integration test in admin

--- admin/src/test/DeleteUserIntegration.test.tsx
+++ admin/src/test/DeleteUserIntegration.test.tsx
@@ -3,32 +3,6 @@ import { MemoryRouter, Route, Routes } from "react-router-dom";
 import UserDetail from "../components/user/UserDetail";
 
 let userId = "";
-const mockUsers = [
-  {
-    email: "john1@incubyte.co",
-    password: "123123123",
-    id: "1",
-    createdAt: Date.prototype,
-    profilePhoto: "https://profilephoto.com",
-    updatedAt: Date.prototype,
-    eId: "E00",
-    role: "BQA",
-    clientTeam: "abc",
-    Role: "Employee",
-  },
-  {
-    email: "john2@incubyte.co",
-    password: "123123123",
-    id: "2",
-    createdAt: Date.prototype,
-    profilePhoto: "https://profilephoto.com",
-    updatedAt: Date.prototype,
-    eId: "E01",
-    role: "BQA",
-    clientTeam: "abc",
-    Role: "Employee",
-  },
-];
 
 beforeAll(async () => {
   const response = await fetch(
@@ -59,21 +33,6 @@ beforeAll(async () => {
   });
   const authToken = await res.text();
   localStorage.setItem("authToken", authToken);
-
-  jest.spyOn(window, "fetch").mockImplementation((url): any => {
-    if (url === "https://backend-mu-plum.vercel.app/user/all") {
-      const headers = new Headers();
-      headers.set(
-        "Authorization",
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImVhMTI5YzA0LTRmNWItNDUwZC1hMjY5LTYwZTZlOTc5M2FlZSIsImVtYWlsIjoic2hyZXlhc0BpbmN1Ynl0ZS5jbyIsImlhdCI6MTY4MTQ2NjMzNywiZXhwIjoxNjgxNDY5OTM3fQ.qo4Ek_bgMs_4Hv1iHJLSgVP246B7Y5rAeO-hBBDK87U"
-      );
-      return Promise.resolve({
-        ok: true,
-        json: () => Promise.resolve(mockUsers),
-        headers: headers,
-      });
-    }
-  });
 });
 
 afterAll(async () => {
