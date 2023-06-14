0cd5948fc3ab4de2453b0457b0d562ad3ffbd591
amanramoliya <19ceuot073@ddu.ac.in>
2023-06-14 16:03:00 +0530

    ENH: change the reset link in email service

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -92,7 +92,7 @@ export class AuthService {
         to: useremail,
         from: 'a.learningplanner@gmail.com',
         subject: 'Reset Password LearningPlanner@Incubyte',
-        html: `<p>click <a href="http://localhost:4000/auth/reset_password/${token}"> here </a> to reset your password</p>`,
+        html: `<p>click <a href="https://admin-incubyte-learningplanner.netlify.app/auth/reset_password/${token}"> here </a> to reset your password</p>`,
       });
 
       await this.prismaService.forgotPassword.create({
@@ -140,7 +140,7 @@ export class AuthService {
         to: useremail,
         from: 'a.learningplanner@gmail.com',
         subject: 'Reset Password LearningPlanner@Incubyte',
-        html: `<p>click <a href="http://localhost:3000/auth/reset_password/${token}">here </a>to reset your password</p>`,
+        html: `<p>click <a href="https://incubyte-learningplanner.netlify.app/auth/reset_password/${token}">here </a>to reset your password</p>`,
       });
 
       await this.prismaService.forgotPassword.create({
