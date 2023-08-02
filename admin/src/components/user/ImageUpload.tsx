282c9fcd349e59010682dce02347b022d847cd68
Charvit123 <charvitardeshna@gmail.com>
2023-08-02 16:07:59 +0530

    ENH:adds image compression in admin

--- admin/src/components/user/ImageUpload.tsx
+++ admin/src/components/user/ImageUpload.tsx
@@ -1,24 +1,41 @@
-export const imageUpload = async (images:any) => {
-    let imgArr = [];
-    for(const item of images){
+import imageCompression from "browser-image-compression";
+
+export const imageUpload = async (images: any) => {
+  let imgArr = [];
+
+  if (images.size > 0) {
+    throw new Error("no image selected");
+  } else {
+    for (const item of images) {
+      if (item.size > 7 * 1024 * 1024) {
+        throw new Error("size should be less then 7 mb");
+      } else {
         const formData = new FormData();
 
-        if(item.camera){
-            formData.append("file", item.camera);
-        }else{
-            formData.append("file", item);
-        }
+        const compressedFile = await imageCompression(item, { maxSizeMB: 0.5 });
+
+        formData.append("file", compressedFile);
 
         formData.append("upload_preset", "jxxkhti2");
+
         formData.append("cloud_name", "dxepcudkt");
 
-        const res = await fetch("https://api.cloudinary.com/v1_1/dxepcudkt/upload", {
+        const res = await fetch(
+          "https://api.cloudinary.com/v1_1/dxepcudkt/upload",
+
+          {
             method: "POST",
-            body: formData
-        });
+
+            body: formData,
+          }
+        );
 
         const data = await res.json();
-        imgArr.push({public_id: data.public_id, url: data.secure_url});
+
+        imgArr.push({ public_id: data.public_id, url: data.secure_url });
+      }
+
+      return imgArr;
     }
-    return imgArr;
+  }
 };
