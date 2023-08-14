3c90f647442b7bab254161f16e3b0f0d00e8e77c
Aayush Prajapati <ashprajapati67@gmail.com>
2023-08-14 12:04:50 +0530

    ENH:adds radiobutton

--- admin/src/components/user/AddUserForm.tsx
+++ admin/src/components/user/AddUserForm.tsx
@@ -11,6 +11,7 @@ export const AddUserForm = () => {
   const [designation, setDesignation] = useState("");
   const [clientTeam, setclientTeam] = useState("");
   const [role, setRole] = useState("Employee");
+  const [selectedOption, setSelectedOption] = useState("option2");
   const navigator = useNavigate();
   const handleCreateUser = async () => {
     const authToken = localStorage.getItem("authToken");
@@ -77,6 +78,9 @@ export const AddUserForm = () => {
   const addUser = async () => {
     await setShowModal(true);
   };
+  const handleRadioButtonChange = (event: any) => {
+    setSelectedOption(event.target.value);
+  };
   const [showModal, setShowModal] = useState(false);
 
   return (
@@ -88,116 +92,138 @@ export const AddUserForm = () => {
         isUser={true}
         isTag={false}
       ></Navbar>
-      {showModal ? (
-        <>
-          <AddUser setShowModal={setShowModal} showModal={showModal} />
-        </>
-      ) : null}
-      <div>
-        <h1 className="AddUserHeading" data-testid="addMultipleUsersHeading">
-          Add Multiple Users
-        </h1>
-      </div>
-      <div className="FormContainer">
-        <button
-          data-testid="addMultipleUserBtn"
-          className="AddUserSubmitButton bg-blue-500"
-          type="submit"
-          onClick={addUser}
-        >
-          Add Multiple Users
-        </button>
-      </div>
-      <div>
-        <h1 className="AddUserHeading" data-testid="addSingleUserHeading">
+      <div className="text-center pt-10 text-xl font-bold">
+        <label className="mx-3 cursor-pointer">
+          <input
+            type="radio"
+            value="option1"
+            checked={selectedOption === "option1"}
+            onChange={handleRadioButtonChange}
+            className="mx-1"
+          />
+          Add Multiple User
+        </label>
+        <label className="mx-3 cursor-pointer">
+          <input
+            type="radio"
+            value="option2"
+            checked={selectedOption === "option2"}
+            onChange={handleRadioButtonChange}
+            className="mx-1"
+          />
           Add Single User
-        </h1>
+        </label>
       </div>
-      <div className="FormContainer">
-        <form
-          data-testid="formContainer"
-          onSubmit={handleSubmit}
-          className="AddUserContainer"
-        >
-          <div className="FormGroup">
-            <label className="AddUserTextLabel">Email</label>
-            <input
-              className="AddUserTextInput"
-              data-testid="emailInput"
-              type="text"
-              placeholder="Enter email id"
-              required
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="FormGroup">
-            <label className="AddUserTextLabel">Employee ID</label>
-            <input
-              className="AddUserTextInput"
-              type="text"
-              data-testid="employeeIdInput"
-              placeholder="Enter employee id"
-              required
-              value={employeeId}
-              onChange={(e) => setEmployeeId(e.target.value)}
-            />
-          </div>
-          <div className="FormGroup">
-            <label className="AddUserTextLabel">Designation</label>
-            <input
-              className="AddUserTextInput"
-              type="text"
-              data-testid="designationInput"
-              placeholder="Enter designation"
-              required
-              value={designation}
-              onChange={(e) => setDesignation(e.target.value)}
-            />
-          </div>
-          <div className="FormGroup">
-            <label className="AddUserTextLabel">Client Team</label>
-            <input
-              className="AddUserTextInput"
-              type="text"
-              data-testid="clientTeamInput"
-              placeholder="Enter client team"
-              required
-              value={clientTeam}
-              onChange={(e) => setclientTeam(e.target.value)}
-            />
-          </div>
-          <div className="FormGroup">
-            <label className="AddUserTextLabel">Role</label>
-            <div>
-              <select
-                name="select role"
-                className="AddUserTextInput"
-                data-testid="roleSelect"
-                required
-                onChange={handleRoleChange}
-              >
-                <option value="Employee" data-testid="roleSelectOption1">
-                  Employee
-                </option>
-                <option value="Admin" data-testid="roleSelectOption2">
-                  Admin
-                </option>
-              </select>
-            </div>
-          </div>
-          <div className="flex justify-center">
+
+      {selectedOption === "option1" && (
+        <>
+          {showModal ? (
+            <>
+              <AddUser setShowModal={setShowModal} showModal={showModal} />
+            </>
+          ) : null}
+
+          <div className="FormContainer">
             <button
-              data-testid="submitButton"
+              data-testid="addMultipleUserBtn"
               className="AddUserSubmitButton bg-blue-500"
               type="submit"
+              onClick={addUser}
             >
-              Add User
+              Add Multiple Users
             </button>
           </div>
-        </form>
-        <ToastContainer></ToastContainer>
-      </div>
+        </>
+      )}
+      {selectedOption === "option2" && (
+        <>
+          <div className="FormContainer">
+            <form
+              data-testid="formContainer"
+              onSubmit={handleSubmit}
+              className="AddUserContainer"
+            >
+              <div className="FormGroup">
+                <label className="AddUserTextLabel">Email</label>
+                <input
+                  className="AddUserTextInput"
+                  data-testid="emailInput"
+                  type="text"
+                  placeholder="Enter email id"
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="FormGroup">
+                <label className="AddUserTextLabel">Employee ID</label>
+                <input
+                  className="AddUserTextInput"
+                  type="text"
+                  data-testid="employeeIdInput"
+                  placeholder="Enter employee id"
+                  required
+                  value={employeeId}
+                  onChange={(e) => setEmployeeId(e.target.value)}
+                />
+              </div>
+              <div className="FormGroup">
+                <label className="AddUserTextLabel">Designation</label>
+                <input
+                  className="AddUserTextInput"
+                  type="text"
+                  data-testid="designationInput"
+                  placeholder="Enter designation"
+                  required
+                  value={designation}
+                  onChange={(e) => setDesignation(e.target.value)}
+                />
+              </div>
+              <div className="FormGroup">
+                <label className="AddUserTextLabel">Client Team</label>
+                <input
+                  className="AddUserTextInput"
+                  type="text"
+                  data-testid="clientTeamInput"
+                  placeholder="Enter client team"
+                  required
+                  value={clientTeam}
+                  onChange={(e) => setclientTeam(e.target.value)}
+                />
+              </div>
+              <div className="FormGroup">
+                <label className="AddUserTextLabel">Role</label>
+                <div>
+                  <select
+                    name="select role"
+                    className="AddUserTextInput"
+                    data-testid="roleSelect"
+                    required
+                    onChange={handleRoleChange}
+                  >
+                    <option value="Employee" data-testid="roleSelectOption1">
+                      Employee
+                    </option>
+                    <option value="Admin" data-testid="roleSelectOption2">
+                      Admin
+                    </option>
+                  </select>
+                </div>
+              </div>
+              <div className="flex justify-center">
+                <button
+                  data-testid="submitButton"
+                  className="AddUserSubmitButton bg-blue-500"
+                  type="submit"
+                >
+                  Add User
+                </button>
+              </div>
+            </form>
+            <ToastContainer></ToastContainer>
+          </div>
+        </>
+      )}
     </>
   );
 };
